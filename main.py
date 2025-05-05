f5f446d901a9dab7e78e19ece934f171f8514e9e
Prateek Kumar <prateekkumar72007@gmail.com>
2025-05-06 02:18:02 +0530

    Added some basics of tuples, list, sets, if-else and match-case

--- main.py
+++ main.py
@@ -43,3 +43,66 @@ She is a good girl'''
 print(a1)
 print(a2)
 print(a3)
+
+#  lists
+
+l1 = [2,56,34523,33,3,33,"Prateek"]
+print(l1)
+print(type(l1))
+l1.remove("Prateek") #it will only remove 1st instance of 33
+print(l1)
+print(len(l1))
+l1.sort()  # Sort the list in-place
+l1.append(1234)
+print(l1)  # Print the sorted list
+
+# tuples they are immutable
+t1 = (3,78,95,132,653)
+print(t1.count(78))
+print(t1.index(95))
+
+# sets (duplicates are not considered)
+s1 = {2,6,6,6,7,99,4,6}
+s2 = {2,8,99,4,8,0,12}
+s1.pop() # randomly removes a no. from set
+print(s1)
+print(s1.union(s2))
+print(s1.intersection(s2))
+
+# Dictionaries
+dict1 = {}
+s3 =set()
+print(dict1,type(dict1))
+print(s3,type(s3))
+
+dict2 = {"Prateek":65,"Tanu":95,"Dev":75,"Bhogesh":58,"Saurabh":31};
+print(dict2["Saurabh"])
+
+# if else
+
+age = int(input("Enter your age: "))
+
+if(age>18):
+    print("Yes, you can drive")
+elif(age==1):
+    print("You are a kid")
+elif(age==10):
+    print("You are a decade kid")
+else:
+    print("No, you can go home")
+print("End of the program")
+
+# match case
+m1 = int(input("Please enter a case no."))
+
+match m1:
+    case 1:
+        print("Case is 1")
+    case 2:
+        print("Case is 2")
+    case 3:
+        print("Case is 3")
+    case 4:
+        print("Case is 4")
+    case _:
+        print("No case found")
